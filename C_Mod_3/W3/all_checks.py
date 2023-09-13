633569f22b10af007bf8d70e0b9acd135b31be8c
MpAngieS <angelica.macias@autozone.com>
2023-09-13 12:40:29 -0600

    Rename min_absolute to min_gb, use parameter names

--- C_Mod_3/W3/all_checks.py
+++ C_Mod_3/W3/all_checks.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python 
+
 import shutil 
 import sys
+import os 
 
-def check_dick_usage(disk, min_absolut, min_percent):
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def check_dick_full(disk, min_gb, min_percent):
     """Returns True if there is enough free disk space, false otherwise"""
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total 
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolut:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return True 
     return False
 
 def main():
 
-    if not check_dick_usage("/", 2, 10):
+    if check_reboot():
+        print("Pending Reboot")
+        sys.exit(1)
+    if check_reboot("/", 2, 10):
         print("ERROR: not enough disk space")
         sys.exit(1)
-
     print("Everything ok")
     sys.exit(0)
+
+main()

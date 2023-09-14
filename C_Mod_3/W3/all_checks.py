f32c3019c37126bc3bd9b2c42f50c7e0020bf516
MpAngieS <angelica.macias@autozone.com>
2023-09-14 12:38:06 -0600

    Allow printing more that one error message

--- C_Mod_3/W3/all_checks.py
+++ C_Mod_3/W3/all_checks.py
@@ -26,11 +26,13 @@ def main():
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
     ]
-
+    everying_ok = True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok= False
+    if not everying_ok:
+        sys.exit(1)
 
     print("Everything ok")
     sys.exit(0)
